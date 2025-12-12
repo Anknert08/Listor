6e8fe318f8e8eebc16889df013725101f7422eeb
Anknert08 <ebbe@aknert.se>
2025-12-12 13:19:28 +0100

    listor

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+List<double> numbers = new List<double>{3.2,3.7,4.1,2.6,6.2};
+numbers.RemoveAt(3);
+
+foreach (double number in numbers)
+{
+    Console.WriteLine(number);
+}
+average();
+
+
+double average()
+{
+    double sum = 0;
+    foreach (double number in numbers)
+    {
+        sum = sum += number;
+    }
+    
+    return sum / numbers.Count;
+}
